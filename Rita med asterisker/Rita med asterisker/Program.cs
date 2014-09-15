422d94f774000c1855f8839b8933df1ea422cfd4
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-15 12:56:01 +0200

    skapat projekt

---
+++ Rita med asterisker/Rita med asterisker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rita_med_asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
