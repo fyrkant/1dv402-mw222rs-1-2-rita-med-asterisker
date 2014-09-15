5abbcae3528e57b64bf1837721d2df080cf582c5
Mattias Wikström <mw222rs@student.lnu.se>
2014-09-15 22:10:01 +0200

    börjat med A-nivå

--- Rita med asterisker/Rita med asterisker/Program.cs
+++ Rita med asterisker/Rita med asterisker/Program.cs
@@ -10,6 +10,32 @@ namespace Rita_med_asterisker
     {
         static void Main(string[] args)
         {
+            for (int rad = 2; rad <= 25; rad++)
+            {
+                if (rad % 2 != 0)
+                {
+                    Console.Write(" ");
+                }
+
+                    for (int kolumn = 2; kolumn <= 39; kolumn++)
+                    {
+
+                        Console.Write("* ");
+                    }
+
+                Console.WriteLine();
+            }
+
+
+
         }
+        //private static byte ReadOddByte()
+        //{
+            
+        //}
+        //private static void RenderTriangle(byte cols)
+        //{
+
+        //}
     }
 }
